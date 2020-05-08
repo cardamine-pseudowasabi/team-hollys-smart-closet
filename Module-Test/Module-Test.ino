9e3a22903b940dd17133cb80a68a635d1b79e546
y0s30b <coacervate@g.skku.edu>
2020-05-09 01:16:06 +0900

    스텝 모터(KH4234-B90102), L298N 모터 드라이버 작동 테스트

---
+++ Module-Test/Module-Test.ino
@@ -0,0 +1,21 @@
+// Stepper module test
+#include <Stepper.h>
+
+const int stepsPerRevolution = 50;
+const int stepsPerRevolution = 52;
+// 50 = 90도
+// 52 -> 오차 보정
+
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+// 순서대로 8, 9, 10, 11번 핀
+
+void setup() {
+    myStepper.setSpeed(180);
+    // 각도를 줄이면 속도를 높이고
+    // 각도를 늘리면 속도를 줄여도 잘 돌아가는 듯
+}
+
+void loop() {
+    myStepper.step(stepsPerRevolution);
+    delay(2000);
+}
